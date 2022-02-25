b10046bdd45bb35b233e679fb6c3e272c84b21b9
morevq <xhenulik5@gmail.com>
2022-02-25 20:05:45 +0300

    first1

--- app/src/main/java/ru/samsung/itschool/dbgame/DBManager.java
+++ app/src/main/java/ru/samsung/itschool/dbgame/DBManager.java
@@ -3,6 +3,7 @@ package ru.samsung.itschool.dbgame;
 import java.io.File;
 import java.util.ArrayList;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -32,8 +33,11 @@ public class DBManager {
 	}
 
 	void addResult(String username, int score) {
-		db.execSQL("INSERT INTO RESULTS VALUES ('" + username + "', " + score
-				+ ");");
+		//db.execSQL("INSERT INTO RESULTS VALUES ('" + username + "', " + score + ");");
+		ContentValues contentValues = new ContentValues();
+		contentValues.put("USERNAME", username);
+		contentValues.put("SCORE", score);
+		db.insert("RESULTS", null, contentValues);
 	}
 
 
@@ -42,6 +46,44 @@ public class DBManager {
 
 		ArrayList<Result> data = new ArrayList<Result>();
 		Cursor cursor = db.rawQuery("SELECT * FROM RESULTS;", null);
+		boolean hasMoreData = cursor.moveToFirst();
+
+		while (hasMoreData) {
+			String name = cursor.getString(cursor.getColumnIndex("USERNAME"));
+			int score = cursor.getInt(1);//Integer.parseInt(cursor.getInt(cursor.getColumnIndex("SCORE")));
+			data.add(new Result(name, score));
+			hasMoreData = cursor.moveToNext();
+		}
+
+		return data;
+	}
+
+	ArrayList<Result> getAllUserResults(String username, int minScore) {
+
+		ArrayList<Result> data = new ArrayList<Result>();
+
+		Cursor cursor = db.rawQuery("SELECT * FROM RESULTS WHERE USERNAME=? AND SCORE >= ?", new String[]{username, minScore + ""});
+		//Cursor cursor1 = db.rawQuery("SELECT USERNAME || 'abc', SCORE*SCORE FROM RESULTS WHERE USERNAME=? AND SCORE >= ?", new String[]{username, minScore + ""});
+		//создает два столбца юзернеймabc и скоре умноженное на скоре
+
+		boolean hasMoreData = cursor.moveToFirst();
+
+		while (hasMoreData) {
+			String name = cursor.getString(cursor.getColumnIndex("USERNAME"));
+			int score = Integer.parseInt(cursor.getString(cursor
+					.getColumnIndex("SCORE")));
+			data.add(new Result(name, score));
+			hasMoreData = cursor.moveToNext();
+		}
+
+		return data;
+	}
+	ArrayList<Result> getMaxUserResults(String username, int minScore) {
+		Cursor cursor = db.query("RESULTS",
+				new String[]{"USERNAME", "MAX(SCORE) AS MS"},
+				null, null, "USERNAME",
+				"MS > 500" , "MS DESC");
+		ArrayList<Result> data = new ArrayList<Result>();
 		boolean hasMoreData = cursor.moveToFirst();
 
 		while (hasMoreData) {
